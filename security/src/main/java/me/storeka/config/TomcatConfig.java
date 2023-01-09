5bb7a4e92338649cc15433f567ebe13c0f7dd6ce
mohammedtarek <mohammedtarek74@yahoo.com>
2023-01-10 00:04:50 +0200

    Edited redirection to be conditional based on ssl enabled flag

--- security/src/main/java/me/storeka/config/TomcatConfig.java
+++ security/src/main/java/me/storeka/config/TomcatConfig.java
@@ -5,6 +5,7 @@ import org.apache.catalina.connector.Connector;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -13,12 +14,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class TomcatConfig {
 
-    @Value("${server.httpPort}")
+    @Value("${server.redirectFromPort}")
     int httpPort;
-    @Value("${server.port}")
+    @Value("${server.ssl.port}")
     int httpsPort;
 
     @Bean
+    @ConditionalOnProperty(value="server.ssl.enabled", havingValue = "true", matchIfMissing = true)
     public ServletWebServerFactory servletContainer() {
         TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
             @Override
@@ -32,12 +34,14 @@ public class TomcatConfig {
                 context.addConstraint(securityConstraint);
             }
         };
+
+        tomcat.setPort(httpsPort);
         tomcat.addAdditionalTomcatConnectors(redirectConnector());
         return tomcat;
     }
 
     private Connector redirectConnector() {
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
         connector.setScheme("http");
         connector.setPort(httpPort);
         connector.setSecure(false);
